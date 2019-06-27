0d11ff8db8ed82fd77e1633def1b3008719a6415
Raul <raul.sjbv@gmail.com>
2019-06-27 17:23:02 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:tela_login/ui/login.dart';
+
+void main() => runApp(
+
+      new MaterialApp(
+  home: new Login(),
+) // MaterialApp
+);
+
+
+
+
+
+
+
+
